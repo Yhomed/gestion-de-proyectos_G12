c87f45dd0e4535faa7841053dc1fb4080e2d109d
abrilalvarez <amoedomonica@yahoo.com.ar>
2022-10-26 16:31:29 -0300

    hojas de estilos

--- app.js
+++ app.js
@@ -12,22 +12,19 @@ app.listen(process.env.PORT || 3000, () => console.log('Servidor activo.'));
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
-}
-)
+})
 
 app.get("/productCart", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
-}
-)
+})
 
 app.get("/productDetail", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
-}
-)
+})
+
 app.get("/login", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
-}
-)
+})
 
 app.post("/register", (req,res) => {
     res.redirect("/");
