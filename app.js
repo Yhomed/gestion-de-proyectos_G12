c2390586446c8516d1705046b07df606bb979616
abrilalvarez <amoedomonica@yahoo.com.ar>
2022-10-26 15:51:44 -0300

    archivo app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+// app.use("/static", express.static(__dirname + "/public"));
+
+app.listen(process.env.PORT || 3000, () => console.log('Servidor activo.'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+}
+)
+
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
+}
+)
+
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
+}
+)
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+}
+)
+
+app.post("/register", (req,res) => {
+    res.redirect("/");
+})
+
+app.post("/login", (req,res) => {
+    res.redirect("/");
+})
