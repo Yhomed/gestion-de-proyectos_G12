2660d629bee626347b379a9afe803c6e79373394
abrilalvarez <amoedomonica@yahoo.com.ar>
2022-10-26 16:42:58 -0300

    archivo app.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-// app.use("/static", express.static(__dirname + "/public"));
-
 app.listen(process.env.PORT || 3000, () => console.log('Servidor activo.'));
 
 app.get("/", (req, res) => {
