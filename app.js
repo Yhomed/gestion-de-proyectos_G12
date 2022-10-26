f4e1fe35fd12983e3f57cdc7ec25df374adb4fba
FernandoSanchez-z <fer30801@gmail.com>
2022-10-26 18:41:09 -0300

    aa

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, () => console.log('Servidor activo.'));
 
+
+
+
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 })
