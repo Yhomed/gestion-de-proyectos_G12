4bbb004f8bbb227c5c8dbd63c1ce5608a4a5b15b
FernandoSanchez-z <fer30801@gmail.com>
2022-10-27 17:30:25 -0300

    indexHTML/CSS

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000, () => console.log('Servidor activo.'));
+app.listen(process.env.PORT || 3050, () => console.log('Servidor activo.'));
 
 
 
