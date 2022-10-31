95de062897610a665446e21458c7c250fd469566
LCanasE <lcanase@gmail.com>
2022-10-30 19:04:41 -0500

    html inscription

--- app.js
+++ app.js
@@ -3,20 +3,7 @@ const express = require('express');
 const path = require('path');
 
 
-document.addEventListener('DOMContentLoaded', ()=> { fetchData() });
-const app = express();
-const fetchData = async () => {
-    try {
-      const res = await fetch('api.json')
-      const data = awai .json ();
-      console.log(data)
-    } catch (error) {
-        console.log(error)
-    }
-    }
-const pintarProductos = (data) => {
 
-}
 
 app.use(express.static('public'));
 // app.use(express.static(path.resolve(__dirname, './public'))); ¿Cuál es la diferencia entre usar esta línea o la anterior?
