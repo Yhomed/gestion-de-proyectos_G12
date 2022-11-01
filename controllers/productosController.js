54650b9d8cf87c91f454b0ccceaec333a9ad29f5
LCanasE <lcanase@gmail.com>
2022-11-01 00:23:46 -0500

    corrección estilos

---
+++ controllers/productosController.js
@@ -0,0 +1,9 @@
+const productosController = {
+
+    listaDeProductos: (req, res) => res.send('Indez de productos'),
+
+    crearProducto: function() {},
+    detalleDeProducto: function() {},
+};
+
+module.exports = productosController;
