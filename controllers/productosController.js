2b0061459c95ae8395d4ff5af92853f416e69fe7
abrilalvarez <amoedomonica@yahoo.com.ar>
2022-11-30 15:44:06 -0300

    agregado 7 rutas

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -1,18 +1,32 @@
 const path = require('path');
 const fs = require('fs');
 const { receiveMessageOnPort } = require('worker_threads');
+
+//variable que recupera los datos de productos.json 
+
 let products = fs.readFileSync(path.join(__dirname, '../data/productos.json'), 'utf8');
 products = JSON.parse(products);
 
 
 const productosController =   {
 
+    //listado
+    productList: (req, res) => {
 
+        res.render('productList', {products}); //a la variable se la pasa como objeto literal
+
+    },
+
+    //formulario de creación
+    createProducts: (req, res) => res.render('./products/create'), 
+
+    //detalle de un producto
     productDetail: (req, res) => {
 
         let product = products.filter(p => p.id==req.params.id)
 
-        res.render('../views/products/productDetail.ejs', {
+        res.render('../views/products/productDetail.ejs', 
+        {
             title: product[0].title,
             number: product[0].number,
             parrafo: product[0].parrafo
@@ -20,24 +34,17 @@ const productosController =   {
 
     },
 
-    //metodo
-    productList: (req, res) => {
+    //formulario de edición
+    editProducts: (req, res) => res.render('./products/edit'),
 
-        res.render('productList', {products}); //renderiza vista con el listado de productos
+    //acción de creación (post)
+    createNewProduct: (req, res) => res.render(''),
 
-    },
+    //acción de edición (put)
+    editNewProduct: (req, res) => res.render(''),
 
-    createProducts: (req, res) => res.render('./products/create'),
-
-    
-
-    editProducts: (req, res) => res.render('./products/:id/edit'),
-
-
-
-    productCar: (req, res) => res.render('./products/productCar'),
-
-
+    //acción de borrado (delete)
+    deleteProduct: (req, res) => res.render(''),
 
 }
 
--- routes/products.js
+++ routes/products.js
@@ -6,14 +6,28 @@ const multer = require('multer');
 
 const productosController = require('../controllers/productosController');
 
-//ruta 1
+/* 7 RUTAS */
+
+//ruta 1 para el listado de productos 
 router.get('/', productosController.productList);
 
-//ruta 3
-router.get('/:id',productosController.productDetail);
+//ruta 2 para el formulario de creación
+router.get('/create', productosController.createProducts); 
 
-//ruta 5
-router.get(':id/edit', productosController.productDetail);
+//ruta 3 para el detalle de un producto particular
+router.get('/:id',productosController.productDetail); //listo
+
+//ruta 4 para la acción de creación (POST) --> alta
+router.get('/', productosController.createNewProduct);
+
+//ruta 5 para el formulario de edición
+router.get('/:id/edit', productosController.editProducts);
+
+//ruta 6 para la acción de edición (PUT) --> modificación
+router.get('/:id', productosController.editNewProduct);
+
+//ruta 7 para la acción de borrado (DELETE) --> baja
+router.get('/:id', productosController.deleteProduct);
 
 // editProductos
 
