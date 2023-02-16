f1f7e52083ed2a00e8abc4a79d3f4d3caddd020b
FernandoSanchez-z <fer30801@gmail.com>
2023-02-16 19:50:31 -0300

    vistas y controlador

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,7 +36,6 @@ const userController = {
             res.render('./users/detail.ejs',{usuarios});
         })
 
-
     },
 
     //LOGIN FORM
@@ -102,6 +101,7 @@ const userController = {
     },
 
     profile: (req, res) => {
+
         let users = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
         let user = users.filter(p => p.id == req.params.id);
         console.log(user);
@@ -116,6 +116,8 @@ const userController = {
     
     //EDIT FORM
     editUsers: (req, res) => {
+
+        /*
         let users = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
         let user = users.filter(p => p.id==req.params.id)
         res.render('./users/edit.ejs', 
@@ -126,15 +128,25 @@ const userController = {
             email: user[0].email,
             password: user[0].password,
             image: user[0].image,
-
         })
+        */
+
+        db.Usuario.findByPk(req.params.id)
+        .then(function(usuario){
+
+            res.render('./users/edit.ejs',{usuario});
+        })
+    
     },
 
     //ACCIÓN DE EDICIÓN (PUT)
+
+    
     editUser: (req, res) => {
+
+        /*
         let users = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
         req.body.id = Number(req.params.id);
-
         let newUsers = users.map((user) => {
             if (user.id == req.body.id) {
                 let temp = req.body;
@@ -148,35 +160,55 @@ const userController = {
         let updatedUser = JSON.stringify(newUsers, null, 2);
         fs.writeFileSync(path.resolve(__dirname, '../data/usuarios.json'), updatedUser);
         res.redirect('/user/');
+        */
+
+        db.Usuario.update ({
+            name: req.body.name,
+            surname: req.body.surname,
+            password: req.body.password,
+            image: req.file ? req.file.filename : usuario.image,
+            email : req.body.email
+            //categoryId : req.body.categoria
+        }, {
+            where: {  
+                id: req.params.id
+           }
+        })
+        .then(()=> res.redirect('/user'))
+        .catch(error =>res.send(error))
+
     },
 
-    //ACCIÓN DE BORRADO (DELETE)
+    //ACCIÓN DE BORRADO (DELETE) no hacer
     deleteUser: (req, res) => {
+
+        /*
         let users = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
         let userId = req.params.id;
         //trae todos los registros distintos al userId
         let finalUsers = users.filter((product) => product.id != userId);
         let usersToSave = JSON.stringify(finalUsers, null, 2);
         fs.writeFileSync(path.resolve(__dirname, '../data/usuarios.json'), usersToSave);
-    res.redirect('/user/');
+        res.redirect('/user/');
+        */
+
+        db.Usuario.destroy({
+            where: {
+                id : req.params.id
+            }
+        })
+        .then(()=>  res.redirect('/user'))
+        .catch(error => res.send(error))
     },
 
     //DELETE FORM
     deleteUsers: (req, res) => {
-        let users = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
-        let user = users.filter(p => p.id==req.params.id)
-        //console.log(user);
-        res.render('./users/delete.ejs', 
-        {
-            id: user[0].id,
-            nombre: user[0].title,
-            apellido: user[0].number,
-            email: user[0].email,
-            password: user[0].password,
-            image: user[0].image,
+
+        db.Usuario.findByPk(req.params.id)
+        .then(function(usuario){
+
+            res.render('./users/delete.ejs',{usuario});
         })
     }
 
 };
-
-module.exports = userController; 
