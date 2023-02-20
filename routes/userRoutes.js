7d6d9f67973c44905b35e0f38e06170c26484325
yhomed durango <yhomedurango@gmail.com>
2023-02-19 19:10:57 -0500

    Validacion registro User

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -37,7 +37,56 @@ router.get('/login', userController.login); // Login
 
 router.get('/inscripcion', userController.inscripcion); // Inscripción
 
-router.post('/', uploadFile.single("image"), userController.registerProcess); //registerProcess
+User.findAll()
+    .then((users) => {
+router.post('/', uploadFile.single("image"),[
+  //Aquí incoporé otras validaciones, para que las tengan de guía para sus proyectos  
+  check('name').isLength({
+        min: 1
+      }).withMessage('El campo nombre no puede estar vacío'),
+  check('surname').isLength({min: 1   
+      }).withMessage('El campo apellido no puede estar vacío'),
+  check('email').isEmail().withMessage('Agregar un email válido'),
+
+  //Aquí valido el Password   
+  check('password').isLength({min: 8 }).withMessage('La contraseña debe tener un mínimo de 8 caractéres al menos una letra y un número'),
+  //Aquí valido si usuario existe o no en la tabla de usuarios Por el campo email)
+  body('email').custom(function (value) {
+    let contador = 0;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email == value) {
+            contador++;
+        }
+    }
+    if (contador > 0) {
+        return false;   
+    } else {
+        return true;    
+    }
+  }).withMessage('Usuario ya se encuentra registrado'),
+
+body('image').custom(function (value, { req }) {
+  let ext
+  if(req.file != undefined ){
+      return true
+  }else{
+      ext = ""+path.extname(req.files[0].filename).toLowerCase();
+  }
+  if (
+      ext == ".jpg" ||
+      ext == ".jpeg" ||
+      ext == ".png" ||
+      ext == ".gif"){
+          return true;
+      }
+      return false;
+}).withMessage('Solo debe seleccionar archivos  con extensión JPG, JPEG, PNG o GIF')
+], userController.registerProcess);//registerProcess
+    })
+.catch((errors) => {
+  console.log(errors);
+})  
+
 
 router.post('/profile',[
   check('email').isEmail().withMessage('Email invalido'),
